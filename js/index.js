d96406d14ee6dd936e03f659bef4f91c58dc36ab
ndrijephte <ndrijephtekoffi@gmail.com>
2026-02-26 01:16:38 +0000

    Intégration de l'essentiel du site - reste la mise à jour des images et la page réalisation

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@ window.addEventListener("scroll", () => {
     btnContainer.style.opacity = 1;
   }
 });
+
+// Gestion des transitions
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    const href = this.href;
+
+    if (!href.includes("#")) {
+      e.preventDefault();
+
+      document.body.classList.add("fade-out");
+
+      setTimeout(() => {
+        window.location.href = href;
+      }, 500);
+    }
+  });
+});
